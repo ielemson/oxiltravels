da217228d286e74419448ea454edb1ffc0f6b0da
ielemson <ielemson@gmail.com>
2022-07-15 14:01:25 +0100

    updated dashboard asset folder

--- app/Views/layouts/dashboardLayout.php
+++ app/Views/layouts/dashboardLayout.php
@@ -8,14 +8,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Dashboard | Oxil Travels</title>
     <link rel="icon" href="<?=base_url('frontend/images/favicon.png')?>" type="image/x-icon">
-    <link href="<?=base_url("dashboard/assets/vendor/fontawesome/css/fontawesome.min.css")?>" rel="stylesheet">
-    <link href="<?=base_url("dashboard/assets/vendor/fontawesome/css/solid.min.css")?>" rel="stylesheet">
-    <link href="<?=base_url("dashboard/assets/vendor/fontawesome/css/brands.min.css")?>" rel="stylesheet">
-    <link href="<?=base_url("dashboard/assets/vendor/bootstrap/css/bootstrap.min.css")?>" rel="stylesheet">
+    <link href="<?=base_url("dashboard/assets/fontawesome/css/fontawesome.min.css")?>" rel="stylesheet">
+    <link href="<?=base_url("dashboard/assets/fontawesome/css/solid.min.css")?>" rel="stylesheet">
+    <link href="<?=base_url("dashboard/assets/fontawesome/css/brands.min.css")?>" rel="stylesheet">
+    <link href="<?=base_url("dashboard/assets/bootstrap/css/bootstrap.min.css")?>" rel="stylesheet">
     <link href="<?=base_url("dashboard/assets/css/master.css")?>" rel="stylesheet">
     <link href="<?=base_url("dashboard/assets/css/lightbox.min.css")?>" rel="stylesheet">
-    <link href="<?=base_url("dashboard/assets/vendor/flagiconcss/css/flag-icon.min.css")?>" rel="stylesheet">
-    <link href="<?=base_url("dashboard/assets/vendor/datatables/datatables.min.css")?>" rel="stylesheet">
+    <link href="<?=base_url("dashboard/assets/flagiconcss/css/flag-icon.min.css")?>" rel="stylesheet">
+    <link href="<?=base_url("dashboard/assets/datatables/datatables.min.css")?>" rel="stylesheet">
    
     <?= $this->renderSection('extra_style'); ?>
 </head>
@@ -27,13 +27,13 @@
         <!-- ========================= SECTION MAIN END// ========================= -->
     </div>
     </div>
-    <script src="<?=base_url("dashboard/assets/vendor/jquery/jquery.min.js")?>"></script>
-    <script src="<?=base_url("dashboard/assets/vendor/bootstrap/js/bootstrap.bundle.min.js")?>"></script>
-    <script src="<?=base_url("dashboard/assets/vendor/chartsjs/Chart.min.js")?>"></script>
+    <script src="<?=base_url("dashboard/assets/jquery/jquery.min.js")?>"></script>
+    <script src="<?=base_url("dashboard/assets/bootstrap/js/bootstrap.bundle.min.js")?>"></script>
+    <script src="<?=base_url("dashboard/assets/chartsjs/Chart.min.js")?>"></script>
     <script src="<?=base_url("dashboard/assets/js/dashboard-charts.js")?>"></script>
     <script src="<?=base_url("dashboard/assets/js/script.js")?>"></script>
     <script src="<?=base_url("dashboard/assets/js/form-validator.js")?>"></script>
-    <script src="<?=base_url("dashboard/assets/vendor/datatables/datatables.min.js")?>"></script>
+    <script src="<?=base_url("dashboard/assets/datatables/datatables.min.js")?>"></script>
     <script src="<?=base_url("dashboard/assets/js/initiate-datatables.js")?>"></script>
     <script src="<?=base_url("dashboard/assets/js/lightbox.min.js")?>"></script>
 </body>
